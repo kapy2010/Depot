1ed09a8c5bb73f43a8ad64bf7433237213e68d9f
Kapil Singh <singh.kapy@gmail.com>
2017-08-21 13:18:18 +0200

    Checkout

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -6,7 +6,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     @update = {
       title: 'Lorem Ipsum',
       description: 'Wibbles are fun!',
-      image_url: 'lorem.jpg',
+      image_url: '7apps.jpg',
       price: 19.95
     }
   end
